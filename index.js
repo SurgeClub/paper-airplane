20dcd8b7100e3f659c06ba7122e0ff186cce8574
mmahalwy <mohamed@peek.com>
2016-02-27 17:57:28 -0800

    logs

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const schedule = require('node-schedule');
 
 const triggerMessages = require('./triggerMessages');
 
+console.log('------------App started----------------');
+
 schedule.scheduleJob('*/30 * * * *', function() {
   console.log('Job scheduled');
   triggerMessages();
