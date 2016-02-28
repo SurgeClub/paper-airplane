e5adcc4cb49e8cf2a0fbd7a9df8b93f0b2af5d77
mmahalwy <mohamed@peek.com>
2016-02-27 16:47:19 -0800

    by the second

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 var schedule = require('node-schedule');
 
-schedule.scheduleJob('45 * * * *', function() {
-  console.log('The answer to life, the universe, and everything!');
+schedule.scheduleJob('5 * * * * *', function() {
+  console.log('The answer to life, the universe, and everything!5');
 });
-schedule.scheduleJob('15 * * * *', function() {
-  console.log('The answer to life, the universe, and everything!');
+schedule.scheduleJob('15 * * * * *', function() {
+  console.log('The answer to life, the universe, and everything!15');
 });
-
-schedule.scheduleJob('30 * * * *', function() {
-  console.log('The answer to life, the universe, and everything!');
+schedule.scheduleJob('30 * * * * *', function() {
+  console.log('The answer to life, the universe, and everything!30');
 });
-
-schedule.scheduleJob('59 * * * *', function() {
-  console.log('The answer to life, the universe, and everything!');
+schedule.scheduleJob('45 * * * * *', function() {
+  console.log('The answer to life, the universe, and everything!45');
 });
