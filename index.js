474ad13a2dccc61dfb2c64fa52e4cab229bb3584
mmahalwy <mohamed@peek.com>
2016-02-27 17:55:33 -0800

    cron made

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-var schedule = require('node-schedule');
+require('dotenv').load();
+const schedule = require('node-schedule');
 
-schedule.scheduleJob('5 * * * * *', function() {
-  console.log('The answer to life, the universe, and everything!5');
-});
-schedule.scheduleJob('15 * * * * *', function() {
-  console.log('The answer to life, the universe, and everything!15');
-});
-schedule.scheduleJob('30 * * * * *', function() {
-  console.log('The answer to life, the universe, and everything!30');
-});
-schedule.scheduleJob('45 * * * * *', function() {
-  console.log('The answer to life, the universe, and everything!45');
+const triggerMessages = require('./triggerMessages');
+
+schedule.scheduleJob('*/30 * * * *', function() {
+  console.log('Job scheduled');
+  triggerMessages();
 });
---
+++ triggerMessages.js
@@ -0,0 +1,55 @@
+const Firebase = require('firebase');
+const moment = require('moment');
+const sinchSms = require('sinch-sms')({
+	key: process.env.SINCH_KEY,
+	secret: process.env.SINCH_SECRET
+});
+
+module.exports = function() {
+  const firebase = new Firebase(process.env.FIREBASE_URL);
+
+  function parseDate(date) {
+    return parseInt(date.format('YYYYMMDDHHmm'), 10);
+  };
+
+  function formatDate(dateNum) {
+    const dateString = String(dateNum);
+
+    return moment(`${dateString.slice(0, 8)}T${dateString.slice(8)}`);
+  };
+
+  firebase.child('events')
+    .orderByChild('startTime')
+    .startAt(parseDate(moment()))
+    .endAt(parseDate(moment().add(2, 'hour')))
+  .on('value', function(snapshot) {
+    const events = snapshot.val();
+
+    const eventsText = Object.keys(events)
+    .sort(function(prev, next) {
+      return formatDate(prev.startTime).isBefore(formatDate(next.startTime)) ? -1 : 1;
+    })
+    .map(function(eventId) {
+      const event = events[eventId];
+
+      return `${event.category}: ${formatDate(event.startTime).format('hh:mmA')}\n${event.address}`
+    }).join('\n');
+
+    firebase.child('users').on('value', function(snapshot) {
+      const users = snapshot.val();
+
+      Object.keys(users).map(function(userId) {
+        const user = users[userId];
+
+        console.log(`Sending to ${user.phoneNumber}`);
+        sinchSms.send(`+1${user.phoneNumber}`, eventsText).then(function(response) {
+        	//All good, response contains messageId
+        	console.log(response);
+        }).fail(function(error) {
+        	// Some type of error, see error object
+        	console.log(error);
+        });
+      });
+    });
+  });
+}
